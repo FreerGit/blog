e78de03d3cd1066b3b8b34268934541695907f3e
FreerGit <fredrik-98@live.se>
2024-03-22 03:45:52 +0100

    feat: starting of SIMD and code highlighting

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-import { marked } from 'marked';
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from 'highlight.js/lib/core';
+import hljsZig from "highlightjs-zig";
+require("./public/zig.css")
+
+hljs.registerLanguage("zig", hljsZig);
+hljs.registerLanguage('plaintext', require('highlight.js/lib/languages/plaintext'));
+
+// hljs.initHighlightingOnLoad();
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -6,6 +16,7 @@ const ejs = require('ejs');
 
 const app = express();
 const PORT = 3000;
+
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -17,7 +28,17 @@ fs.readdir('./posts', (err, files) => {
     const name = file.split('.')[0];
     const filePath = path.join(import.meta.dir, 'posts', file);
     const fileContents = fs.readFileSync(filePath, 'utf8');
-    
+    const marked = new Marked(
+      markedHighlight({
+        langPrefix: 'hljs language-',
+        highlight(code, lang, info) {
+          console.log("lang",lang)
+          const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+          console.log("language", language)
+          return hljs.highlight(code, { language }).value;
+        }
+      })
+    );
     const html = marked.parse(fileContents);
     app.get(`/${name}`, (req, res) => {
       res.render('post', { title: name, content: html });
